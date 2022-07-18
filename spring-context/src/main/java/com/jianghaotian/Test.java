b33414c6be12faff51e42789fcb63142b2e00976
蒋浩天 <jianghaotian@neurongenius.com>
2022-07-18 23:08:01 +0800

    1.梳理bean创建流程

--- spring-context/src/main/java/com/jianghaotian/Test.java
+++ spring-context/src/main/java/com/jianghaotian/Test.java
@@ -14,19 +14,32 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test {
 	public static void main(String[] args) {
 		//Bean的生成过程
+		//1.扫描 doScan()
+		//2.实例化 createBean()
+		//3.依赖注入 populateBean()
+		//4.初始化 initializeBean()
 
 		//所有后处理器的执行顺序
-		//0. BeanFactoryPostProcessor.postProcessBeanFactory()----扫描包并解析为元数据(ASM技术),符合要求的,封装为bean定义进行注册[核心方法 doScan()]
-		//1. InstantiationAwareBeanPostProcessor.postProcessBeforeInstantiation()---基本不用[核心方法 createBean()]
+		//-----------------------------------------doScan()-----------------------------------------
+		//0. BeanFactoryPostProcessor.postProcessBeanFactory()----扫描包并解析为元数据(ASM技术),符合要求的,封装为bean定义进行注册
+
+		//-----------------------------------------createBean()-----------------------------------------
+		//1. InstantiationAwareBeanPostProcessor.postProcessBeforeInstantiation()---基本不用
+
+		//-----------------------------------------doCreateBean()-----------------------------------------
 		//2. 实例化----推断构造方法[核心方法 doCreateBean()]
-		//3. MergedBeanDefinitionPostProcessor.postProcessMergedBeanDefinition()---查找@Autowired的注入点并缓存[核心方法 doCreateBean()]
-		//4. InstantiationAwareBeanPostProcessor.postProcessAfterInstantiation()---基本不用[核心方法 populateBean()]
-		//5. 自动注入---设置对象的属性值[核心方法 populateBean()]
-		//6. InstantiationAwareBeanPostProcessor.postProcessProperties()---处理@Autowired,@Resource,@Value等注解[核心方法 populateBean()]
-		//7. Aware对象----各种Aware接口的回调[核心方法 initializeBean()]
-		//8. BeanPostProcessor.postProcessBeforeInitialization()----执行@PostConstruct方法,另一些Aware接口的回调[核心方法 initializeBean()]
-		//9. 初始化-----如果实现了InitializingBean接口,就调用其afterPropertiesSet() 方法[核心方法 initializeBean()]
-		//10. BeanPostProcessor.postProcessAfterInitialization()----基于AOP的处理,返回的是代理bean对象[核心方法 initializeBean()]
+		//3. MergedBeanDefinitionPostProcessor.postProcessMergedBeanDefinition()---查找@Autowired的注入点并缓存
+
+		//-----------------------------------------populateBean()-----------------------------------------
+		//4. InstantiationAwareBeanPostProcessor.postProcessAfterInstantiation()---基本不用
+		//5. 自动注入---设置对象的属性值
+		//6. InstantiationAwareBeanPostProcessor.postProcessProperties()---处理@Autowired,@Resource,@Value等注解
+
+		//-----------------------------------------initializeBean()-----------------------------------------
+		//7. Aware对象----各种Aware接口的回调
+		//8. BeanPostProcessor.postProcessBeforeInitialization()----执行@PostConstruct方法,另一些Aware接口的回调
+		//9. 初始化-----如果实现了InitializingBean接口,就调用其afterPropertiesSet() 方法
+		//10. BeanPostProcessor.postProcessAfterInitialization()----基于AOP的处理,返回的是代理bean对象
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		//创建bean
 		UserService userService = (UserService) context.getBean("userService");
