7b0a38c33196e540225d2c3c39257823a123b0c3
蒋浩天 <jianghaotian@neurongenius.com>
2022-07-29 00:03:54 +0800

    1.梳理推断构造方法 2.梳理依赖注入

---
+++ spring-context/src/main/java/com/jianghaotian/service/OrderService.java
@@ -0,0 +1,21 @@
+package com.jianghaotian.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * 描述:
+ * 公司: 纽睿科技
+ * 项目: spring
+ * 创建时间: 2022/7/20 21:25
+ *
+ * @author jianghaotian
+ */
+@Component
+public class OrderService {
+
+	public OrderService orderService(UserService userService) {
+		return new OrderService();
+	}
+}
